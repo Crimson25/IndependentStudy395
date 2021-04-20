698a358bec171d053d2bed54ebfe1468c898775f
Colin <fitzpatrick4937@uwlax.edu>
2021-04-19 21:06:52 -0500

    Changed and updated sorters to include time
    Signed-off-by: Colin <fitzpatrick4937@uwlax.edu>

--- Sorters.java
+++ Sorters.java
@@ -58,20 +58,7 @@ public class Sorters {
    
   }
   
-    public void printArray(int[] n) { 
-    System.out.print("["); 
-    for(int i = 0; i < n.length; i++) { 
-      if (i < n.length-1) { 
-        System.out.print(n[i] + ", "); 
-        } 
-    else { 
-    System.out.print(n[i]); 
-       } 
-      } 
-    System.out.println("]"); 
-    } 
-
-    public void printArray(float[] n) { 
+    public void printArray(long[] n) { 
       System.out.print("["); 
       for(int i = 0; i < n.length; i++) { 
         if (i < n.length-1) { 
@@ -83,23 +70,74 @@ public class Sorters {
         } 
       System.out.println("]"); 
       } 
-     
+          
+    public void averageCalcSel() {
+      int[] arr = new int[] {55,32,21,99,87,62,35,2,14};
+      long[] timeTracker = new long[30];
+      for(int j = 0; j < 30; j++) {
+        long startTime = System.nanoTime();
+        for(int i = 0; i < 1000; i++) {
+          selsort(arr);
+        }
+      long stopTime = System.nanoTime();
+      timeTracker[j] = stopTime - startTime; 
+      }
+      long sum = 0;
+      float average = 0;
+      for (int k = 0; k < timeTracker.length; k++) {
+        sum += timeTracker[k];
+      }
+      average = (float) (sum / 30.0);
+      printArray(timeTracker);
+      System.out.println("The average time for select sort to run is " + average / 1000000000 + " seconds");
+    }
+    
+    public void averageCalcBubble() {
+      int[] arr = new int[] {55,32,21,99,87,62,35,2,14};
+      long[] timeTracker = new long[30];
+      for(int j = 0; j < 30; j++) {
+        long startTime = System.nanoTime();
+        for(int i = 0; i < 1000; i++) {
+          bubbleSort(arr);
+        }
+      long stopTime = System.nanoTime();
+      timeTracker[j] = stopTime - startTime; 
+      }
+      long sum = 0;
+      float average = 0;
+      for (int k = 0; k < timeTracker.length; k++) {
+        sum += timeTracker[k];
+      }
+      average = (float) (sum / 30.0);
+      printArray(timeTracker);
+      System.out.println("The average time for bubble sort to run is " + average / 1000000000 + " seconds");
+    }
+    
+    public void averageCalcBucket() {
+      float[] arr3 = {(float) .42, (float) .21, (float) .55, (float) .88, (float) .67, (float) .05, (float) .37};
+      long[] timeTracker = new long[30];
+      for(int j = 0; j < 30; j++) {
+        long startTime = System.nanoTime();
+        for(int i = 0; i < 1000; i++) {
+          bucketSort(arr3, arr3.length);
+        }
+      long stopTime = System.nanoTime();
+      timeTracker[j] = stopTime - startTime; 
+      }
+      long sum = 0;
+      float average = 0;
+      for (int k = 0; k < timeTracker.length; k++) {
+        sum += timeTracker[k];
+      }
+      average = (float) (sum / 30.0);
+      printArray(timeTracker);
+      System.out.println("The average time for bucket sort to run is " + average / 1000000000 + " seconds");
+    }
 
     public static void main(String[]agrs) { 
     Sorters ob = new Sorters(); 
-    int[] arr = new int[] {55,32,21,99,87,62,35,2,14}; 
-    ob.selsort(arr); 
-    System.out.println("Sortred Array Using Selection Sort:"); 
-    ob.printArray(arr); 
-    
-    int[] arr2 = new int[] {62,31,22,99,87,2,14,55,57};
-    ob.bubbleSort(arr2);
-    System.out.println("Sortred Array Using Bubble Sort:"); 
-    ob.printArray(arr2); 
-    
-    float[] arr3 = {(float) .42, (float) .21, (float) .55, (float) .88, (float) .67, (float) .05, (float) .37};
-    ob.bucketSort(arr3, arr3.length);
-    System.out.println("Sortred Array Using Bucket Sort:");
-    ob.printArray(arr3);
+    ob.averageCalcSel();
+    ob.averageCalcBubble();
+    ob.averageCalcBucket();
     }     
 }
